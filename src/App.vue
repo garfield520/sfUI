6f49e10367c43eb515935f6272e3de51cbe332b9
butel-bing <butel-bing@butel-bingdeMacBook-Air.local>
2017-11-19 22:46:11 +0800

    Remove useless files

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div id="app">
+    <h2>sfUI - test</h2>
+    <hr>
     <div>
-      <sf-button @click="$router.push({name: 'Hello'})">welcome</sf-button>
-      <sf-button @click="goHome">home</sf-button>
-      <sf-button @click="goAbout">about</sf-button>
-      <sf-button @click="goProfile">profile</sf-button>
-      <sf-test></sf-test>
+      <sf-button>default</sf-button>
+      <sf-button type="success">success</sf-button>
     </div>
-    <router-view class="out-router" />
   </div>
 </template>
 
@@ -16,49 +14,15 @@ export default {
   name: 'app',
 
   methods: {
-    goHome () {
-      this.$router.push({
-        name: 'Music',
-        params: {
-          id: 1
-        }
-      })
-    },
 
-    goAbout () {
-      this.$router.push({
-        name: 'About'
-      })
-    },
-
-    goProfile () {
-      this.$router.push({
-        name: 'Profile'
-      })
-    }
   },
 
   mounted () {
-    console.log(document.body.clientHeight)
+
   }
 }
 </script>
 
 <style>
-html, body {
-  height: 100%;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
 
-.out-router {
-  border:1px solid #ccc;
-  margin-top: 20px;
-  padding: 20px;
-  height: 550px;
-}
 </style>
